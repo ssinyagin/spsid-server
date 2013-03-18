17fb3d7e1b852e683b9f0c46d91189b21fb2200f
Stanislav Sinyagin <ssinyagin@k-open.com>
2013-03-18 23:08:45 +0100

    redirect STDERR in test Plack instance

--- t/stop_plack.sh
+++ t/stop_plack.sh
@@ -2,4 +2,4 @@
 echo "Stopping Plack server" 1>&2
 
 kill `cat ${SPSID_PLACK_PIDFILE}`
-rm -f ${SPSID_PLACK_PIDFILE} ${SPSID_PLACK_LOG}
+rm -f ${SPSID_PLACK_PIDFILE} ${SPSID_PLACK_LOG} ${SPSID_PLACK_ERRLOG}
