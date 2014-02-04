246ec055a9a8ef5d5b8cf10bce34974e7b7d0a1b
Stanislav Sinyagin <ssinyagin@k-open.com>
2014-02-04 02:38:59 +0100

    minor fix

--- etc/spsid_siteconfig.pl
+++ etc/spsid_siteconfig.pl
@@ -4,7 +4,7 @@ $SPSID::Config::Backend::SQL::dbi_dsn =
     'DBI:mysql:database=spsid;host=localhost';
 
 $SPSID::Config::Backend::SQL::dbi_user = 'spsid';
-$SPSID::Config::Backend::SQL::dbi_password = 'Uwae4hei',
+$SPSID::Config::Backend::SQL::dbi_password = 'Uwae4hei';
 $SPSID::Config::Backend::SQL::dbi_attr = 
 {'mysql_auto_reconnect' => 1};
 
